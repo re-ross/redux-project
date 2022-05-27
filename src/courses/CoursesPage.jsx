7e3f9156694f01b15afe7fd486fab243319f9130
Ryan Ross <ryan@reross.com>
2022-05-27 10:24:41 -0500

    adding redux - created action, reducer, rootReducer, and store

--- src/courses/CoursesPage.jsx
+++ src/courses/CoursesPage.jsx
@@ -1,7 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 
 const CoursesPage = () => {
-  return <h2>Courses</h2>;
+  const [course, setCourse] = useState({ title: "" });
+  const handleChange = (e) => {
+    const newCourse = { ...course, title: e.target.value };
+    setCourse(newCourse);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert(course.title);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Courses</h2>
+      <h3>Add Course</h3>
+      <input type="text" onChange={handleChange} value={course.title} />
+      <input type="submit" value="Save" />
+    </form>
+  );
 };
 
 export default CoursesPage;
